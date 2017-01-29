8fd307e2b659225e385fa4e4062572762375fbf0
Roinand Aguila <rbaguila1@up.edu.ph>
2017-01-30 01:06:44 +0800

    Added loginbuttons in the top_nav component

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,8 @@
 // Import client startup through a single index entry point
 
 import './routes.js';
+import { Accounts } from 'meteor/accounts-base';
+ 
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
+});
