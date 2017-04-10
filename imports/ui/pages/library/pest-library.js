978601feaef923c69b22eb8ea70abd396240aabf
eleimeguiso <lgmeguiso@up.edu.ph>
2017-04-10 22:45:55 +0800

    Edited pest library (added disease feature)

--- imports/ui/pages/library/pest-library.js
+++ imports/ui/pages/library/pest-library.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import './pest-library.html';
 
 var currentPests = "";
+var currentDiseases = "";
 
 Template.pestsLib.onCreated(function () {
   Meteor.subscribe('pests.all');
@@ -13,17 +14,17 @@ Template.pestsLib.onCreated(function () {
 Template.pestsLib.helpers({
 
 	pestsIndex(){
-		return PestsIndex
+		return PestsIndex;
 	},
 
 	searchAttributes() {
-    return {
-      placeholder: 'Search',
-      class: 'form-control'
-    };
-  },
+	    return {
+	      placeholder: 'Search',
+	      class: 'form-control'
+	    };
+	},
 
-  equals: function(v1, v2) {
+  	equals: function(v1, v2) {
 		return (v1 === v2);
 	},
 
@@ -31,8 +32,8 @@ Template.pestsLib.helpers({
 		return CMS.findOne({info:'finalLib'});
 	},
 
-	setPageSessions(type) {
-		currentPest = type;
+	setPageSessionsPest(type) {
+		currentPest = type + " Pest";
 		var pestCount = Pests.find({'type': 'Pest', 'plant_affected': type}).count();
 		var pestsPerPage = parseInt(CMS.findOne({info:'finalLib'}).pestsPerPage);
 		//var pestsPerPage = 4;
@@ -43,8 +44,28 @@ Template.pestsLib.helpers({
 			// console.log(Session.get(currentPest + " Count"));
 	},
 
+	setPageSessionsDisease(type) {
+		currentDisease = type + " Disease";
+		var diseaseCount = Pests.find({'type': 'Disease', 'plant_affected': type}).count();
+		var diseasesPerPage = parseInt(CMS.findOne({info:'finalLib'}).pestsPerPage);
+		//var pestsPerPage = 4;
+			Session.set(currentDisease, 1);
+
+			diseaseCount = diseaseCount%diseasesPerPage == 0? Math.floor(diseaseCount/diseasesPerPage) : Math.floor(diseaseCount/diseasesPerPage+1);
+			Session.set(currentDisease + " Count", diseaseCount);
+			// console.log(Session.get(currentDisease + " Count"));
+	},
+
 	getCurrentPestType(){
 		return currentPest;
+	},
+
+	getCurrentDiseaseType(){
+		return currentDisease;
+	},
+
+	getCurrentType(){
+		return currentType;
 	},
 
 	displayPest(type){
@@ -53,24 +74,62 @@ Template.pestsLib.helpers({
 		return Pests.find({'type': 'Pest', 'plant_affected': type}, {sort: {name: 1}, skip:(Session.get(currentPest)-1)*pestsPerPage, limit:pestsPerPage});
 	},
 
-	setPagination(type){
-		var count = Session.get(type + " Count");
+	displayDisease(type){
+		var diseasesPerPage = parseInt(CMS.findOne({info:'finalLib'}).pestsPerPage);
+		//var pestsPerPage = 4;
+		return Pests.find({'type': 'Disease', 'plant_affected': type}, {sort: {name: 1}, skip:(Session.get(currentDisease)-1)*diseasesPerPage, limit:diseasesPerPage});
+	},
+
+	setPestPagination(type){
+		var count = Session.get(type + " Pest Count");
 		var pages = [];
 		for(var i = 1; i<=count; i++)
 			pages.push({num: i});
 		return pages;
 	},
 
-	isCurrentPage(page){
+	setDiseasePagination(type){
+		var count = Session.get(type + " Disease Count");
+		var pages = [];
+		for(var i = 1; i<=count; i++)
+			pages.push({num: i});
+		return pages;
+	},
+
+	isCurrentPestPage(page){
 		return Session.equals(currentPest, page);
+	},
+
+	isCurrentDiseasePage(page){
+		return Session.equals(currentDisease, page);
 	}
 });
 
 Template.pestsLib.events({
-	'click .page-number'(event) {
+	'click .page-number-pest'(event) {
 		currentPest = $(event.target).attr("name");
 		Session.set(currentPest, this.num); // stores the current page number of typeOfPest
+		//console.log(currentPest + " page: " + Session.get(currentPest));
+	},
+
+	'click .page-number-disease'(event) {
+		currentDisease = $(event.target).attr("name");
+		Session.set(currentDisease, this.num); // stores the current page number of typeOfDisease
+		//console.log(currentDisease + " page: " + Session.get(currentDisease));
+	},
+
+	'click .pest-lib'(event) {
+		currentType = $(event.target).attr("name");
+		Session.set(currentType, "Pest"); // stores the current page number of typeOfPest
 		// console.log(currentPest + " page: " + Session.get(currentPest));
+		//console.log("Pest");
+	},
+
+	'click .disease-lib'(event) {
+		currentType = $(event.target).attr("name");
+		Session.set(currentType, "Disease"); // stores the current page number of typeOfPest
+		// console.log(currentPest + " page: " + Session.get(currentPest));
+		//console.log("Disease");
 	},
 });
 
