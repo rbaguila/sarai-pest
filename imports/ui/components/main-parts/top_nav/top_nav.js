4b1a23776c847e4908097571fe2de603b4092c1d
Vanessa Romero <vanessamaelromero@gmail.com>
2017-06-09 10:53:56 +0800

    Nav bar with collections

--- imports/ui/components/main-parts/top_nav/top_nav.js
+++ imports/ui/components/main-parts/top_nav/top_nav.js
@@ -1,9 +1,19 @@
 import { Links } from '/imports/api/links/links.js';
 import './top_nav.html';
 
-Template.top_nav.onCreated(function topnavOnCreated() {
+// Template.top_nav.onCreated(function topnavOnCreated() {
+// });
+// Template.top_nav.helpers({
+// });
+// Template.top_nav.events({
+// });
+
+Template.top_nav.onCreated(function() {
+  Meteor.subscribe('links.all');
 });
+
 Template.top_nav.helpers({
-});
-Template.top_nav.events({
+  links: function() {
+    return Links.find({});
+  }
 });
