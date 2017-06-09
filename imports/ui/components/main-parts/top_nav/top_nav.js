bcdb19a054955882630a469b2e521e113f97f9bc
Vanessa Romero <vanessamaelromero@gmail.com>
2017-06-09 10:25:04 +0800

    Updated Navigation bar

--- imports/ui/components/main-parts/top_nav/top_nav.js
+++ imports/ui/components/main-parts/top_nav/top_nav.js
@@ -1,19 +1,9 @@
 import { Links } from '/imports/api/links/links.js';
 import './top_nav.html';
 
-// Template.top_nav.onCreated(function topnavOnCreated() {
-// });
-// Template.top_nav.helpers({
-// });
-// Template.top_nav.events({
-// });
-
-Template.top_nav.onCreated(function() {
-  Meteor.subscribe('links.all');
+Template.top_nav.onCreated(function topnavOnCreated() {
 });
-
 Template.top_nav.helpers({
-  links: function() {
-    return Links.find({});
-  }
+});
+Template.top_nav.events({
 });
