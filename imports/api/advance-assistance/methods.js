4f04903240fbeef2b48d6302ee442a067faaffca
anjmendoza <memendoza3@up.edu.ph>
2017-07-21 00:46:23 +0800

    Fixed Advanced Assistance Form

--- imports/api/advance-assistance/methods.js
+++ imports/api/advance-assistance/methods.js
@@ -10,9 +10,10 @@ Meteor.methods({
     check( newAdvanceAssistance.user, String);
     check( newAdvanceAssistance.date, String);
     check( newAdvanceAssistance.email, String);
+    check( newAdvanceAssistance.name, String);
     check( newAdvanceAssistance.location, String);
-    check( newAdvanceAssistance.source, String);
     check( newAdvanceAssistance.area, String);
+    check( newAdvanceAssistance.source, String);
     check( newAdvanceAssistance.cropStage, String);
     check( newAdvanceAssistance.variety, String);
     check( newAdvanceAssistance.pesttype, String);
@@ -20,16 +21,27 @@ Meteor.methods({
     check( newAdvanceAssistance.parts, String);
     check( newAdvanceAssistance.distribution, String);
     check( newAdvanceAssistance.damage, String);
+    check( newAdvanceAssistance.affectedArea, String);
+    check( newAdvanceAssistance.lengthDays, Number);
+    check( newAdvanceAssistance.lengthWeeks, Number);
+    check( newAdvanceAssistance.lengthMonths, Number);
+    check( newAdvanceAssistance.fertilizer, String);
+    check( newAdvanceAssistance.insecticide, String);
+    check( newAdvanceAssistance.fungicide, String);
+    check( newAdvanceAssistance.herbicide, String);
+    check( newAdvanceAssistance.weather, String);
+    check( newAdvanceAssistance.chemapplied, String);
+    check( newAdvanceAssistance.weatherobserved, String);
 
       advanceAssistance.insert(
         { 
           user: newAdvanceAssistance.user,
           date: newAdvanceAssistance.date,
           email: newAdvanceAssistance.email,
-          
+          name: newAdvanceAssistance.name,
           location: newAdvanceAssistance.location,
-          source: newAdvanceAssistance.source, 
           area: newAdvanceAssistance.area,
+          source: newAdvanceAssistance.source, 
           cropStage: newAdvanceAssistance.cropStage,
           variety: newAdvanceAssistance.variety,      
 
@@ -38,7 +50,10 @@ Meteor.methods({
           parts : newAdvanceAssistance.parts,
           distribution : newAdvanceAssistance.distribution,
           damage : newAdvanceAssistance.damage,
-          
+          affectedArea : newAdvanceAssistance.affectedArea,
+          lengthDays : newAdvanceAssistance.lengthDays,
+          lengthWeeks : newAdvanceAssistance.lengthWeeks,
+          lengthMonths : newAdvanceAssistance.lengthMonths,
           fertilizer: newAdvanceAssistance.fertilizer,
           insecticide: newAdvanceAssistance.insecticide,
           fungicide: newAdvanceAssistance.fungicide, 
